7df6dabbeca30159b2ec35980c46666b2faced1e
yy981-jp <134594870+yy981-jp@users.noreply.github.com>
2025-09-06 17:27:13 +0900

    ブランチ整理

--- AES256GCM.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "master.h"
-
-
-extern CryptoGCM encAES256GCM_sodium(const BIN& key, const BIN& nonce, const BIN& ciphertext, const BIN& aad);
-extern BIN decAES256GCM_sodium(const BIN& key, const BIN& nonce, const BIN& ciphertext, const BIN& aad, const BIN& tag);
-extern CryptoGCM encAES256GCM_cryptopp(const BIN& key, const BIN& iv, const BIN& ciphertext, const BIN& aad);
-extern BIN decAES256GCM_cryptopp(const BIN& key, const BIN& iv, const BIN& ciphertext, const BIN& aad, const BIN& tag);
--- def.cpp
+++
@@ -1,9 +0,0 @@
-#include "def.h"
-
-#include <string>
-
-#include <yy981/env.h>
-
-
-const std::string SD = getEnv("localappdata") + "/yy981/EAD7/";
-const std::string SDM = getEnv("localappdata") + "/yy981/EAD7/master/";
